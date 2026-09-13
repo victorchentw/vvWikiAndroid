b7cef3c671d97d938aac6967a1c9f6526d183954
victorchen <victor.chen.tw@gmail.com>
2026-09-13 20:27:14 +0800

    Release vv知識酷 0.1.10

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,8 +30,8 @@ android {
         applicationId = "com.victor.vvwiki"
         minSdk = 28
         targetSdk = 35
-        versionCode = 10
-        versionName = "0.1.9"
+        versionCode = 11
+        versionName = "0.1.10"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
